f83f11dfc28d196a116b09a1902da3e31fb8b90c
Joshua Tan <josh.tan.tann@gmail.com>
2020-09-15 06:14:31 +0000

    Add test code back in to appease eslint

--- head.js
+++ head.js
@@ -1,21 +1,24 @@
 const assertEqual = function(actual, expected) {
-    if (typeof actual === "string") {
-      if (actual === expected) {
-        console.log(`✅ Assertion Passed: "${actual}" === "${expected}"`);
-      } else {
-        console.log(`🛑 Assertion Failed: "${actual}" === "${expected}"`);
-      }
+  if (typeof actual === "string") {
+    if (actual === expected) {
+      console.log(`✅ Assertion Passed: "${actual}" === "${expected}"`);
     } else {
-      if (actual === expected) {
-        console.log(`✅ Assertion Passed: ${actual} === ${expected}`);
-      } else {
-        console.log(`🛑 Assertion Failed: ${actual} === ${expected}`);
-      }
+      console.log(`🛑 Assertion Failed: "${actual}" === "${expected}"`);
     }
-  };
-  
-  const head = arr => {
-    if (arr[0]) {
-      return arr[0];
+  } else {
+    if (actual === expected) {
+      console.log(`✅ Assertion Passed: ${actual} === ${expected}`);
+    } else {
+      console.log(`🛑 Assertion Failed: ${actual} === ${expected}`);
     }
-  };
+  }
+};
+
+const head = arr => {
+  if (arr[0]) {
+    return arr[0];
+  }
+};
+
+assertEqual(head([5, 6, 7]), 5);
+assertEqual(head(["Hello", "Lighthouse", "Labs"]), "Hello");
