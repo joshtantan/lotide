b7ffd91451a8cf22e8407af11aef7c10be86e252
Joshua Tan <josh.tan.tann@gmail.com>
2020-09-15 06:04:26 +0000

    Remove test suite

--- head.js
+++ head.js
@@ -19,6 +19,3 @@ const assertEqual = function(actual, expected) {
       return arr[0];
     }
   };
-  
-  assertEqual(head([5,6,7]), 5);
-  assertEqual(head(["Hello", "Lighthouse", "Labs"]), "Hello");
