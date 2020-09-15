b8cfeb997755018e85a4f5ba33076f6c6af5e981
Joshua Tan <josh.tan.tann@gmail.com>
2020-09-15 05:54:57 +0000

    Remove unnecessary comments

--- assertEqual.js
+++ assertEqual.js
@@ -18,20 +18,3 @@ const assertEqual = function(actual, expected) {
 // TEST CODE
 assertEqual("Lighthouse Labs", "Bootcamp");
 assertEqual(1, 1);
-
-/*
-If the values match, it should print (console.log) the following: Assertion Passed: [actual] === [expected] (but with the values filled in)
-Otherwise it should print (console.log) the following: Assertion Failed: [actual] !== [expected] (but with the values filled in)
-Some examples:
-
-Assertion Failed: "Lighthouse Labs" !== "Bootcamp"
-Assertion Passed: 1 === 1
-Write out a few calls to it and confirm that it is indeed working as expected.
-
-Test at least the following scenarios:
-
-Comparing identical strings
-Comparing non-identical strings
-Comparing identical numbers
-Comparing non-identical numbers
-*/
