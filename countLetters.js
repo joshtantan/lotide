222c3f02ce0f2eb3f5c032f417de8796d52ee5d4
Joshua Tan <josh.tan.tann@gmail.com>
2020-09-16 21:17:48 +0000

    Implement skeleton code and test suite for countLetters.js

---
+++ countLetters.js
@@ -0,0 +1,35 @@
+const assertEqual = function(actual, expected) {
+  if (typeof actual === "string") {
+    if (actual === expected) {
+      console.log(`✅ Assertion Passed: "${actual}" === "${expected}"`);
+    } else {
+      console.log(`🛑 Assertion Failed: "${actual}" === "${expected}"`);
+    }
+  } else {
+    if (actual === expected) {
+      console.log(`✅ Assertion Passed: ${actual} === ${expected}`);
+    } else {
+      console.log(`🛑 Assertion Failed: ${actual} === ${expected}`);
+    }
+  }
+};
+
+const countLetters = words => {
+  const letterCounter = {};
+
+  return letterCounter;
+};
+
+// Test code
+assertEqual(countLetters("lighthouse in the house"), {
+  l: 1,
+  i: 2,
+  g: 1,
+  h: 4,
+  t: 2,
+  o: 2,
+  u: 2,
+  s: 2,
+  e: 3,
+  n: 1,
+});
