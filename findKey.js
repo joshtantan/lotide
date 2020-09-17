4014afb99ec0d741d100bced1325a60ce6de111d
Joshua Tan <josh.tan.tann@gmail.com>
2020-09-17 22:56:02 +0000

    Implement findKey fn

---
+++ findKey.js
@@ -0,0 +1,44 @@
+const assertEqual = function(actual, expected) {
+  if (typeof actual === "string") {
+    if (actual === expected) {
+      console.log(`✅ Assertion Passed: "${actual}" === "${expected}"`);
+    } else {
+      console.log(`🛑 Assertion Failed: "${actual}" === "${expected}"`);
+    }
+  } else {
+    if (actual === expected) {
+      console.log(`✅ Assertion Passed: ${actual} === ${expected}`);
+    } else {
+      console.log(`🛑 Assertion Failed: ${actual} === ${expected}`);
+    }
+  }
+};
+
+const findKey = (obj, callback) => {
+  if (obj && callback) {
+    for (const key in obj) {
+      if (callback(obj[key])) {
+        return key;
+      }
+    }
+  }
+};
+
+// Test code
+assertEqual(findKey({
+  "Blue Hill": { stars: 1 },
+  "Akaleri":   { stars: 3 },
+  "noma":      { stars: 2 },
+  "elBulli":   { stars: 3 },
+  "Ora":       { stars: 2 },
+  "Akelarre":  { stars: 3 }
+}, x => x.stars === 2), "noma"); // => "noma"
+
+assertEqual(findKey({
+  "Blue Hill": { stars: 1 },
+  "Akaleri":   { stars: 3 },
+  "noma":      { stars: 2 },
+  "elBulli":   { stars: 3 },
+  "Ora":       { stars: 2 },
+  "Akelarre":  { stars: 3 }
+}, x => x.stars === 1), "Blue Hill"); // => "Blue Hill"
