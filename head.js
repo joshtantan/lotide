2accd25d0c70d1baaa02395a69cfde1cc7c90f08
Joshua Tan <josh.tan.tann@gmail.com>
2020-09-21 23:34:42 +0000

    Refactor assertEqual.js and head.js to use export and require and also add test suites for them

--- head.js
+++ head.js
@@ -1,24 +1,7 @@
-const assertEqual = function(actual, expected) {
-  if (typeof actual === "string") {
-    if (actual === expected) {
-      console.log(`✅ Assertion Passed: "${actual}" === "${expected}"`);
-    } else {
-      console.log(`🛑 Assertion Failed: "${actual}" === "${expected}"`);
-    }
-  } else {
-    if (actual === expected) {
-      console.log(`✅ Assertion Passed: ${actual} === ${expected}`);
-    } else {
-      console.log(`🛑 Assertion Failed: ${actual} === ${expected}`);
-    }
-  }
-};
-
 const head = arr => {
   if (arr[0]) {
     return arr[0];
   }
 };
 
-assertEqual(head([5, 6, 7]), 5);
-assertEqual(head(["Hello", "Lighthouse", "Labs"]), "Hello");
+module.exports = head;
