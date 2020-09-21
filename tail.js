378bb6d799e35c5cb9e5ac958cff890fdc7d7635
Joshua Tan <josh.tan.tann@gmail.com>
2020-09-21 23:39:23 +0000

    Refactor tail to use export and use its own test suite

--- tail.js
+++ tail.js
@@ -1,19 +1,3 @@
-const assertEqual = function(actual, expected) {
-  if (typeof actual === "string") {
-    if (actual === expected) {
-      console.log(`✅ Assertion Passed: "${actual}" === "${expected}"`);
-    } else {
-      console.log(`🛑 Assertion Failed: "${actual}" === "${expected}"`);
-    }
-  } else {
-    if (actual === expected) {
-      console.log(`✅ Assertion Passed: ${actual} === ${expected}`);
-    } else {
-      console.log(`🛑 Assertion Failed: ${actual} === ${expected}`);
-    }
-  }
-};
-
 const tail = arr => {
   if (!arr) {
     return;
@@ -24,7 +8,4 @@ const tail = arr => {
   }
 };
 
-// Test Case: Check the original array
-const words = ["Yo Yo", "Lighthouse", "Labs"];
-tail(words); // no need to capture the return value since we are not checking it
-assertEqual(words.length, 3); // original array should still have 3 elements!
+module.exports = tail;
