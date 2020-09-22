b25a1be4b7a1f3ccc5dbd03e128bdfde5295a319
Joshua Tan <josh.tan.tann@gmail.com>
2020-09-22 05:29:52 +0000

    Refactor to import eqArrays and eqObjects

--- assertObjectsEqual.js
+++ assertObjectsEqual.js
@@ -1,51 +1,6 @@
-const eqArrays = (arr1, arr2) => {
-  if (arr1.length === arr2.length) {
-    let i = 0;
+const eqArrays = require("./eqArrays");
+const eqObjects = require("./eqObjects");
 
-    while (i < arr1.length) {
-      if (arr1[i] !== arr2[i]) {
-        console.log(`Return false with arr1[${i}]: ${arr1[i]} and arr2[${i}]: ${arr2[i]}`);
-        return false;
-      }
-
-      i++;
-    }
-    
-    return true;
-  } else {
-    return false;
-  }
-};
-
-// Returns true if both objects have identical keys with identical values.
-// Otherwise you get back a big fat false!
-const eqObjects = function(object1, object2) {
-  const obj1KeyTotal = Object.keys(object1).length;
-  const obj2KeyTotal = Object.keys(object2).length;
-
-  if (obj1KeyTotal === obj2KeyTotal) {
-    for (const key in object1) {
-      // Check if values are both arrays or not
-      if (typeof object1[key] === "object" && typeof object2[key] === "object") {
-        if (!eqArrays(object1[key], object2[key])) {
-          return false;
-        }
-      } else if (typeof object1[key] !== "object" && typeof object2[key] !== "object") {
-        if (object1[key] !== object2[key]) {
-          return false;
-        }
-      } else {
-        return false;
-      }
-    }
-
-    return true;
-  }
-
-  return false;
-};
-
-// FUNCTION IMPLEMENTATION
 const assertObjectsEqual = function(actual, expected) {
   const inspect = require('util').inspect;
 
