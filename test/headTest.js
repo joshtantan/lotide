0f6e47e34c0f5a4b3b9981f6fca79cb2664f18f6
Joshua Tan <josh.tan.tann@gmail.com>
2020-09-22 01:56:13 +0000

    Refactor for linting

--- test/headTest.js
+++ test/headTest.js
@@ -1,4 +1,3 @@
-const assertEqual = require('../assertEqual');
 const head = require("../head");
 const assert = require('chai').assert;
 
@@ -13,6 +12,6 @@ describe("#head", () => {
     assert.strictEqual(head(["Hello", "Lighthouse", "Labs"]), "Hello");
   });
   it('returns "5" for ["5"]', () => {
-    assert.strictEqual(head(['5']), '5'); 
+    assert.strictEqual(head(['5']), '5');
   });
 });
