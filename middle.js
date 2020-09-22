e170009d566588ff18062a10ad219a06d3e6e8ee
Joshua Tan <josh.tan.tann@gmail.com>
2020-09-22 00:12:20 +0000

    Refactor middle to export and use its own test suite

--- middle.js
+++ middle.js
@@ -1,31 +1,3 @@
-const eqArrays = (arr1, arr2) => {
-  if (arr1.length === arr2.length) {
-    let i = 0;
-
-    while (i < arr1.length) {
-      if (arr1[i] !== arr2[i]) {
-        console.log(`Return false with arr1[${i}]: ${arr1[i]} and arr2[${i}]: ${arr2[i]}`);
-        return false;
-      }
-
-      i++;
-    }
-    
-    return true;
-  } else {
-    return false;
-  }
-};
-
-const assertArraysEqual = function(actualArr, expectedArr, expectedResult) {
-  if (eqArrays(actualArr, expectedArr) === expectedResult) {
-    console.log(`✅ Assertion Passed: ${actualArr} === ${expectedArr}`);
-  } else {
-    console.log(`🛑 Assertion Failed: ${actualArr} === ${expectedArr}`);
-  }
-};
-
-// ACTUAL FUNCTION
 const middle = function(array) {
   let midArr = [];
   const arrLength = array.length;
@@ -46,10 +18,4 @@ const middle = function(array) {
   return midArr;
 };
 
-// TEST CODE
-assertArraysEqual(middle([1]), [], true);
-assertArraysEqual(middle([1, 2]), [], true);
-assertArraysEqual(middle([1, 2, 3]), [2], true);
-assertArraysEqual(middle([1, 2, 3, 4, 5]), [3], true);
-assertArraysEqual(middle([1, 2, 3, 4]), [2, 3], true);
-assertArraysEqual(middle([1, 2, 3, 4, 5, 6]), [3, 4], true);
+module.exports = middle;
