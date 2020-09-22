f84fc400594d1fcfd0a5615ffc77f22e1df892fc
Joshua Tan <josh.tan.tann@gmail.com>
2020-09-22 05:43:09 +0000

    Refactor letterPositions.js to require and rebase its test suite

--- letterPositions.js
+++ letterPositions.js
@@ -1,29 +1,5 @@
-const eqArrays = (arr1, arr2) => {
-  if (arr1.length === arr2.length) {
-    let i = 0;
-
-    while (i < arr1.length) {
-      if (arr1[i] !== arr2[i]) {
-        console.log(`Return false with arr1[${i}]: ${arr1[i]} and arr2[${i}]: ${arr2[i]}`);
-        return false;
-      }
-
-      i++;
-    }
-    
-    return true;
-  } else {
-    return false;
-  }
-};
-
-const assertArraysEqual = function(actualArr, expectedArr, expectedResult) {
-  if (eqArrays(actualArr, expectedArr) === expectedResult) {
-    console.log(`✅ Assertion Passed: ${actualArr} === ${expectedArr}`);
-  } else {
-    console.log(`🛑 Assertion Failed: ${actualArr} === ${expectedArr}`);
-  }
-};
+const eqArrays = require("./eqArrays");
+const assertArraysEqual = require("./assertArraysEqual");
 
 const letterPositions = function(sentence) {
   const results = {};
@@ -38,26 +14,5 @@ const letterPositions = function(sentence) {
 
   return results;
 };
-
-// Test code
-let testResults = letterPositions("hello");
-
-assertArraysEqual(testResults["h"], [0], true);
-assertArraysEqual(testResults["e"], [1], true);
-assertArraysEqual(testResults["l"], [2, 3], true);
-assertArraysEqual(testResults["o"], [4], true);
-
-testResults = letterPositions("lighthouse in the house");
-
-assertArraysEqual(testResults["l"], [0], true);
-assertArraysEqual(testResults["i"], [1, 11], true);
-assertArraysEqual(testResults["g"], [2], true);
-assertArraysEqual(testResults["h"], [3, 5, 15, 18], true);
-assertArraysEqual(testResults["t"], [4, 14], true);
-assertArraysEqual(testResults["o"], [6, 19], true);
-assertArraysEqual(testResults["u"], [7, 20], true);
-assertArraysEqual(testResults["s"], [8, 21], true);
-assertArraysEqual(testResults["e"], [9, 16, 22], true);
-assertArraysEqual(testResults["n"], [12], true);
 
 module.exports = letterPositions;
---
+++ test/letterPositionsTest.js
@@ -0,0 +1,36 @@
+const letterPositions = require("../letterPositions");
+const assert = require('chai').assert;
+
+describe("#letterPositions", () => {
+  it('returns {h: [0], e: [1], l: [2, 3], o: [4]} for "hello"', () => {
+    const actual = letterPositions("hello");
+
+    const expected = {
+      h: [0],
+      e: [1],
+      l: [2, 3],
+      o: [4]
+    }
+
+    assert.deepEqual(actual, expected);
+  });
+  it('returns {l: [0], i: [1, 11], g: [2], h: [3, 5, 15, 18], t: [4, 14], o: [6, 19], u: [7, 20], s: [8, 21], e: [9, 16, 22], n: [12]} for "lighthouse in the house"', () => {
+    const actual = letterPositions("lighthouse in the house");
+
+    const expected = {
+      l: [0],
+      i: [1, 11],
+      g: [2],
+      h: [3, 5, 15, 18],
+      t: [4, 14],
+      o: [6, 19],
+      u: [7, 20],
+      s: [8, 21],
+      e: [9, 16, 22],
+      n: [12],
+      " ": [10, 13, 17]
+    }
+
+    assert.deepEqual(actual, expected);
+  });
+});
