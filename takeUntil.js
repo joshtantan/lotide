eee24cb5619bd541c388e40784ced83cec3c885b
Joshua Tan <josh.tan.tann@gmail.com>
2020-09-22 06:36:13 +0000

    Refactor takeUntil.js to require and rebase its test suite

--- takeUntil.js
+++ takeUntil.js
@@ -1,30 +1,3 @@
-const eqArrays = (arr1, arr2) => {
-  if (arr1.length === arr2.length) {
-    let i = 0;
-
-    while (i < arr1.length) {
-      if (arr1[i] !== arr2[i]) {
-        console.log(`Return false with arr1[${i}]: ${arr1[i]} and arr2[${i}]: ${arr2[i]}`);
-        return false;
-      }
-
-      i++;
-    }
-    
-    return true;
-  } else {
-    return false;
-  }
-};
-
-const assertArraysEqual = function(actualArr, expectedArr, expectedResult) {
-  if (eqArrays(actualArr, expectedArr) === expectedResult) {
-    console.log(`✅ Assertion Passed: ${actualArr} === ${expectedArr}`);
-  } else {
-    console.log(`🛑 Assertion Failed: ${actualArr} === ${expectedArr}`);
-  }
-};
-
 const takeUntil = function(arr, callback) {
   const results = [];
 
@@ -38,16 +11,5 @@ const takeUntil = function(arr, callback) {
 
   return results;
 };
-
-// Test code
-const data1 = [1, 2, 5, 7, 2, -1, 2, 4, 5];
-const results1 = takeUntil(data1, x => x < 0);
-assertArraysEqual(results1, [1, 2, 5, 7, 2], true);
-
-console.log('---');
-
-const data2 = ["I've", "been", "to", "Hollywood", ",", "I've", "been", "to", "Redwood"];
-const results2 = takeUntil(data2, x => x === ',');
-assertArraysEqual(results2, ["I've", "been", "to", "Hollywood"], true);
 
 module.exports = takeUntil;
---
+++ test/takeUntilTest.js
@@ -0,0 +1,15 @@
+const takeUntil = require("../takeUntil");
+const assert = require('chai').assert;
+
+describe("#tail", () => {
+  it('returns [1, 2, 5, 7, 2] for [1, 2, 5, 7, 2, -1, 2, 4, 5]', () => {
+    const data = [1, 2, 5, 7, 2, -1, 2, 4, 5];
+    const fn = x => x < 0;
+    assert.deepEqual(takeUntil(data, fn), [1, 2, 5, 7, 2]);
+  });
+  it(`returns ["I've", "been", "to", "Hollywood"] for ["I've", "been", "to", "Hollywood", ",", "I've", "been", "to", "Redwood"]`, () => {
+    const data = ["I've", "been", "to", "Hollywood", ",", "I've", "been", "to", "Redwood"];
+    const fn = x => x === ','
+    assert.deepEqual(takeUntil(data, fn), ["I've", "been", "to", "Hollywood"]);
+  });
+});
